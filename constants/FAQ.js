22538a041da1a776b5486b1c8d01655e5b22714b
SATHVIK SVS <svssathvik77@gmail.com>
2023-11-04 20:58:18 +0530

    faq/about constants separated

---
+++ constants/FAQ.js
@@ -0,0 +1,28 @@
+const faq = [
+    {
+      key: 1,
+      question : "What is the main motto of HungerHelp?",
+      answer : "Together, we are tackling hunger and making a difference in our community. At HungerHelp, we connect local food establishments, food banks, shelters, and individuals in need, creating a seamless platform to reduce food waste and address food insecurity. Through our web application, you can easily donate surplus food, volunteer your time, and ensure that no one goes hungry. Join us in this meaningful mission and make a positive impact on the lives of others."
+    },
+    {
+      key : 2,
+      question : "Does HungerHelp expect any kind of remuneration?",
+      answer : "Introducing 'HungerHelp' - a compassionate non-profit organization dedicated to eradicating hunger's plight. With unwavering determination and your support, we strive to bring hope and nourishment to those in need. Join us in our mission to create a world where no one goes to bed hungry."
+    },
+    {
+      key : 3,
+      question : "Contact for any design suggestions",
+      answer : "For any design queries or technical contact kindly reach sathvikworkmail@gmail.com. For any feedback related elements feel free to use the feedback form :)"
+    },
+    {
+      key : 4,
+      question : "Do i get paid if I join in HungerHelp?",
+      answer : "HungerHelp is purely a non-profit organisation which has been running on our self interest for now without any financial aid, So we would love to take volunteers who works for love :)"
+    },
+    {
+      key : 5,
+      question : "How can I join as a volunteer?",
+      answer : "Thanks for your interest please navigate to join page."
+    }
+]
+export default faq;
